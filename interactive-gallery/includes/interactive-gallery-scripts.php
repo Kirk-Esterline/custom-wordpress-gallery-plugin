919ccd43c87f0a121d9a7c9416c29b4cdd4ffb11
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-04-05 14:47:23 -0400

    Updated existing syntax and prefixes to follow best practices

--- interactive-gallery/includes/interactive-gallery-scripts.php
+++ interactive-gallery/includes/interactive-gallery-scripts.php
@@ -1,8 +1,9 @@
 <?php
 // add scripts
-function ig_add_scripts(){
+// main prefix defined as iag
+function iag_add_scripts(){
     // Add Main CSS
-    wp_enqueue_style('ig-main-style', plugins_url(). '/interactive-gallery/css/style.css');
+    wp_enqueue_style('iag-main-style', plugins_url(). '/interactive-gallery/css/style.css');
     // Add Main JS
-    wp_enqueue_script('ig-main-script', plugins_url(). '/interative-gallery/js/main.js');
+    wp_enqueue_script('iag-main-script', plugins_url(). '/interative-gallery/js/main.js');
 }
