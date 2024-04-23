52809a9acc1984acdb55e61374d5ca3815c42d4a
Capn-Cricket <111900268+Capn-Cricket@users.noreply.github.com>
2024-04-23 17:00:39 -0400

    added banner at the top of the page notting it as a test site.

--- interactive-gallery/interactive-gallery.php
+++ interactive-gallery/interactive-gallery.php
@@ -13,3 +13,26 @@ Author URI: https://esterenkodesign.com
 if(!defined('ABSPATH')) {
     exit;
 }
+
+//Test functions to gain some comfort with PHP and how this works with WP
+
+add_action('wp_body_open', 'tb_head');
+add_action('wp_print_styles', 'tb_css');
+
+function tb_head()
+{
+    echo '<h1 class="tb">This is a test site!</h1>';
+}
+
+function tb_css()
+{
+    echo '
+        <style>
+            h1.tb {
+                color: #000;
+                text-align: center;
+                background: #fff;
+            }
+        </style>
+    ';
+}
